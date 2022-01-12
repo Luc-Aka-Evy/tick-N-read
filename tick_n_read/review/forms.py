afaddb010a1185e314438517625c8b8265fe9cc3
AkaSan242 <Lucaka@icloud.com>
2022-01-12 09:28:36 +0100

    modification des models d'application authentication et review

---
+++ tick_n_read/review/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.db.models import fields
+
+from . import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = models.Ticket
+        fields = ['title', 'description', 'image']
+
+
+class CreateReviewForm(forms.ModelForm):
+    class Meta:
+        model = models.Review
+        fields = ['ticket', 'rating', 'headline', 'body']
+
+
+class AddReviewForm(forms.ModelForm):
+    class Meta:
+        model = models.Review
+        fields = ['rating', 'headline', 'body']
